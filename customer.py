d499c4278b1465d0e9c75664f21ba09558bfbbbc
minjyun3011 <grand3canyon10@gmail.com>
2023-09-25 13:37:48 +0900

    C-3, 条件分岐が反映されない

--- customer.py
+++ customer.py
@@ -1,17 +1,36 @@
 class Customer:
-    def __init__(self, first_name, family_name):
+    def __init__(self, first_name, family_name, age):
         self.first_name = first_name
         self.family_name = family_name
         self.full_name = f"{first_name} {family_name}"
+        self.age = age
+
+    def age_judge(self):
+        if (self.age < 20):
+            return f"{1000}"
+        elif (20 <= self.age < 65):
+            return f"{1500}"
+        elif (65 <= self.age):
+            return f"{1200}"
 
     def __str__(self):
         return f"{self.full_name}"
+        return f"{self.age}"
 
 
-ken = Customer(first_name="Ken", family_name="Tanaka")
-tom = Customer(first_name="Tom", family_name="Ford")
+ken = Customer(first_name="Ken", family_name="Tanaka", age="15")
+tom = Customer(first_name="Tom", family_name="Ford", age="57")
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age="73")
+
 
 print(ken)
 print(tom)
 
+print(ken.age)
+print(tom.age)
+print(ieyasu.age)
+
+print(ken.age_judge)
+print(tom.age_judge)
+print(ieyasu.age_judge)
 
